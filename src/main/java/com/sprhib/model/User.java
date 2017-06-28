0f352c0a604dff91e6352ee1cebdff0d33edb961
guptatanvi <tanvigupta1124@gmail.com>
2017-06-28 13:20:55 +0530

    changes in user.java

--- src/main/java/com/sprhib/model/User.java
+++ src/main/java/com/sprhib/model/User.java
@@ -1,11 +1,10 @@
 package com.sprhib.model;
 
-import org.omg.CosNaming.NamingContextExtPackage.StringNameHelper;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.criteria.CriteriaBuilder;
 import java.io.Serializable;
 import java.lang.Double;
 
@@ -22,45 +21,51 @@ public class User implements Serializable {
     private String email;
     private String pw;
     private Double phno;
+    private float latitude;
+    private float longitude;
 
-
-    public Integer getId() {
-        return id;
-    }
 
     public void setId(Integer id) {
         this.id = id;
     }
-
-    public String getName() {
-        return name;
-    }
-
     public void setName(String name) {
         this.name = name;
     }
-
-    public String getEmail() {
-        return email;
-    }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
-    public String getPw() {
-        return pw;
-    }
-
     public void setPw(String pw) {
         this.pw = pw;
     }
-
-    public Double getPhno() {
-        return phno;
-    }
-
     public void setPhno(Double phno) {
         this.phno = phno;
     }
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+    public void setLongitude() {
+       this.longitude=longitude;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPw() {
+        return pw;
+    }
+    public Double getPhno() {
+        return phno;
+    }
+    public float getLatitude(){return latitude;}
+    public float getLongitude(){
+        return longitude;
+    }
+
+
 }
