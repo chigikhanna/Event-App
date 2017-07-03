e5b1cb810857aa07663db287c56e6a018fd80ee2
chigikhanna <ck26041998@gmail.com>
2017-07-03 13:25:53 +0530

    Category Controller

--- src/main/java/com/ht/event/controller/CategoryController.java
+++ src/main/java/com/ht/event/controller/CategoryController.java
@@ -6,6 +6,7 @@ import com.ht.event.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -38,5 +39,14 @@ public class CategoryController {
 
         String json = new Gson().toJson(modelAndView);
         return json;
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public ModelAndView deleteCategory(@PathVariable String id){
+        ModelAndView modelAndView=new ModelAndView("home");
+        categoryService.deleteCategory(Integer.parseInt(id));
+        String message="Successfully deleted.";
+        modelAndView.addObject("message",message);
+        return modelAndView;
     }
 }
