d63dccb3a3ff61c7772768efb90e96f831fa610f
guptatanvi <tanvigupta1124@gmail.com>
2017-07-06 16:47:50 +0530

    minor changes

--- src/main/java/com/ht/event/controller/CategoryController.java
+++ src/main/java/com/ht/event/controller/CategoryController.java
@@ -11,10 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-/**
- * Created by chigi on 03-Jul-17.
- */
-
 @Controller
 @RequestMapping(value="/category")
 public class CategoryController {
